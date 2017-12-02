51609679abf191214e8078775322965558f7765c
MackHeller <mack.heller@mail.utoronto.ca>
2017-12-01 22:02:26 -0500

    better output

--- data.rkt
+++ data.rkt
@@ -1,12 +1,13 @@
-;(#lang rosette
+;(
+#lang rosette
                                                       (require racket/match)
                                                       (require rosette/lib/synthax)
                                                       (require racket/include)(include "ults.rkt")
                                                       (define-synthax(gen-expression (booleanvariables ...) (integervariables ...) height)
                                                         #:base (choose booleanvariables ... integervariables ...)
-                                                        #:else (choose
-                                                                booleanvariables ... integervariables ...
-                                                                ((choose >= > <= < + - && ||) (gen-expression (booleanvariables ...)  (integervariables ...) (- height 1))
-                                                                                              (gen-expression (booleanvariables ...)  (integervariables ...) (- height 1)))
-                                                                )
-                                                        ) (define-symbolic a boolean?) (define-symbolic b boolean?) (define (gen a b) (gen-expression (a b) () 1)) (print-forms (synthesize #:forall (list a b) #:guarantee (assert (eq? (or a b) (or (gen-expression (a b) () 0) (gen-expression (a b) () 0)))))) ;)
+                                                        #:else (choose #t #f booleanvariables ... integervariables ...
+                                                               ((choose >= > <= < + - && ||) (gen-expression (booleanvariables ...)  (integervariables ...) (- height 1))
+                                                                                             (gen-expression (booleanvariables ...)  (integervariables ...) (- height 1)))
+                                                               (! (gen-expression (booleanvariables ...)  (integervariables ...) (- height 1)))
+                                                               )
+                                                        ) (define-symbolic a boolean?) (define-symbolic b boolean?) (define-symbolic c boolean?) (define (gen a b c) (gen-expression (a b c) () 2)) (syntax->datum (car (generate-forms (synthesize #:forall (list a b c) #:guarantee (assert (eq? (and (or (and (or a b) (and b c)) (and b (or a c))) (and b (or (and a c) (or c b)))) (gen a b c))))))) ;)
